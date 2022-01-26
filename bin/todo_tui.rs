8a3ae59e4a51a4f2f95e0e9ce8a7aa2cd32e0ea0
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2022-01-26 12:19:03 -0500

    continue dev on tui

--- bin/todo_tui.rs
+++ bin/todo_tui.rs
@@ -13,18 +13,24 @@ use crossterm::terminal::EnterAlternateScreen;
 use crossterm::terminal::LeaveAlternateScreen;
 use std::io::Error as IOError;
 use std::io::stdout as get_stdout;
+use std::io::Stdout;
 use std::path::PathBuf;
 use std::thread::sleep as thread_sleep;
+use std::thread::spawn as thread_spawn;
 use std::time::Duration;
+use std::time::Instant;
+use std::sync::Mutex;
+use std::sync::Arc;
 use tui::backend::CrosstermBackend;
 use tui::widgets::Block;
 use tui::widgets::Borders;
 use tui::Terminal;
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 struct Args {
     #[clap(short, long)]
     list_path: String,
 }
+#[derive(Clone)]
 struct Ctx {
     args: Args,
     path: PathBuf,
@@ -55,31 +61,122 @@ impl GetPath for Ctx {
         &mut self.path
     }
 }
+struct TerminalManager {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+    ctx: Ctx,
+}
+impl TerminalManager {
+    fn init(ctx: Ctx) -> Result<TerminalManager, IOError> {
+        enable_raw_mode()?;
+        let mut stdout = get_stdout();
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+        let backend = CrosstermBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)?;
+        terminal.clear()?;
+        Ok(Self { terminal, ctx, })
+    }
+    fn close(&mut self) -> Result<(), IOError> {
+        disable_raw_mode()?;
+        execute!(
+            self.terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture,
+        )?;
+        self.terminal.show_cursor()?;
+        Ok(())
+    }
+    fn redraw(&mut self) -> Result<(), IOError> {
+        let container = Container::load(&mut self.ctx);
+        self.terminal.draw(|f| {
+            let size = f.size();
+            let block = Block::default()
+                .title("Block")
+                .borders(Borders::ALL);
+            f.render_widget(block, size);
+        })?;
+        Ok(())
+    }
+}
+const TICK_MS: u128 = 200;
+fn wait_if_lt_tick(instant: Instant) {
+    if instant.elapsed().as_millis().lt(&TICK_MS) {
+        let ms = instant.elapsed().as_millis();
+        thread_sleep(Duration::from_millis((TICK_MS - ms) as u64));
+    }
+}
 fn main() -> Result<(), IOError> {
-    let args = Args::parse();
-    let mut ctx = Ctx::new(args);
+    let mut ctx;
+    { // construct ctx
+        let args = Args::parse();
+        ctx = Ctx::new(args);
+    }
     ctx.construct_path();
-    let container = Container::load(&mut ctx);
-    enable_raw_mode()?;
-    let mut stdout = get_stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
-    thread_sleep(Duration::from_millis(5000));
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture,
-    )?;
-    terminal.show_cursor()?;
-    Ok(())
+    // main vars
+    let terminal = Arc::new(Mutex::new(TerminalManager::init(ctx)?));
+    let running = Arc::new(Mutex::new(true));
+    let t1_terminal = terminal.clone();
+    let t1_running = running.clone();
+    // terminal thread
+    thread_spawn(move || { // t1
+        let terminal = t1_terminal;
+        let running = t1_running;
+        loop {
+            let loop_start = Instant::now();
+            { // check running lock
+                let running_lock = match running.lock() {
+                    Ok(l) => l,
+                    Err(_) => return,
+                };
+                if !*running_lock {
+                    return;
+                }
+            }
+            let mut lock = match terminal.lock() {
+                Ok(l) => l,
+                Err(_) => {
+                    let mut running_lock = match running.lock() {
+                        Ok(l) => l,
+                        Err(_) => return,
+                    };
+                    *running_lock = false;
+                    return;
+                },
+            };
+            match lock.redraw() {
+                Ok(_) => {},
+                Err(_) => {
+                    let mut running_lock = match running.lock() {
+                        Ok(l) => l,
+                        Err(_) => return,
+                    };
+                    *running_lock = false;
+                    return;
+                },
+            }
+            wait_if_lt_tick(loop_start);
+        }
+    });
+    // check loop
+    loop {
+        let loop_start = Instant::now();
+        let mut running_lock = match running.lock() {
+            Ok(l) => l,
+            Err(_) => return Ok(()),
+        };
+        if !*running_lock {
+            let mut term_lock = match terminal.lock() {
+                Ok(t) => t,
+                Err(_) => return Ok(()),
+            };
+            match term_lock.close() {
+                Ok(_) => {},
+                Err(_) => {
+                    *running_lock = false;
+                    return Ok(());
+                },
+            }
+            return Ok(());
+        }
+        wait_if_lt_tick(loop_start);
+    }
 }
