c2bd4f26ce93180668add942a68f4566843b6c77
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2022-02-01 13:49:25 -0500

    fix trait function

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,5 +1,5 @@
 use {
-    crossterm::QueueableCommand,
+    crossterm::Command,
     std::{
         io::Error as IOError,
         path::PathBuf,
@@ -9,10 +9,7 @@ pub trait GetPath {
     fn get_path(&self) -> &PathBuf;
     fn get_path_mut(&mut self) -> &mut PathBuf;
 }
-pub trait Terminal
-where
-    Self: std::io::Write
-{
-    fn queue_cmd(&self, cmd: impl QueueableCommand) -> Result<(), IOError>;
+pub trait Terminal {
+    fn queue_cmd(&self, cmd: impl Command) -> Result<(), IOError>;
     fn write_str(&self, msg: impl AsRef<str>) -> Result<(), IOError>;
 }
